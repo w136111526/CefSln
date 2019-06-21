ea4dd82a19a343f7b25978ec495088aa2db67abe
soDLL <32153301+soDLL@users.noreply.github.com>
2019-06-21 16:20:14 +0800

    modify

--- CefMFC/CefMFC.cpp
+++ CefMFC/CefMFC.cpp
@@ -75,9 +75,13 @@ BOOL CCefMFCApp::InitInstance()
 	{
 		CefEnableHighDPISupport();
 
+
 		CefMainArgs main_args(m_hInstance);
-		int exit_code = CefExecuteProcess(main_args, NULL, NULL);
-		//ASSERT(exit_code < 0);
+		int exitCode = CefExecuteProcess(main_args, NULL, NULL);
+		if (exitCode >= 0)
+		{
+			return exitCode;
+		}
 		
 		// 注意：CefSettings的 multi_threaded_message_loop 变量必须设置为true
 		// （当你开发的工程是基于Win32窗口），只有控制台程序才可以设置为false。
